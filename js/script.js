0a7cec9307c13f6a8aed22a046375a9923512f10
bans.cowboy@gmail.com <bans.cowboy@gmail.com>
2021-11-05 16:43:09 +0100

    asd

--- js/script.js
+++ js/script.js
@@ -1,154 +1,197 @@
-$(document).ready(function () {
-	'use strict';
+(function ($) {
+  'use strict';
+
+  /* ========================================================================= */
+  /*	Page Preloader
+  /* ========================================================================= */
+
+  // window.load = function () {
+  // 	document.getElementById('preloader').style.display = 'none';
+  // }
+
+  $(window).on('load', function () {
+    $('#preloader').fadeOut('slow', function () {
+      $(this).remove();
+    });
+  });
+
+  
+  //Hero Slider
+  $('.hero-slider').slick({
+    autoplay: true,
+    infinite: true,
+    arrows: true,
+    prevArrow: '<button type=\'button\' class=\'prevArrow\'></button>',
+    nextArrow: '<button type=\'button\' class=\'nextArrow\'></button>',
+    dots: false,
+    autoplaySpeed: 7000,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+  $('.hero-slider').slickAnimation();
 
 
-	// Shuffle js filter and masonry
-	var containerEl = document.querySelector('.shuffle-wrapper');
-	if (containerEl) {
-		var Shuffle = window.Shuffle;
-		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-			itemSelector: '.shuffle-item',
-			buffer: 1
-		});
+  /* ========================================================================= */
+  /*	Header Scroll Background Change
+  /* ========================================================================= */
 
-		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-			var input = evt.currentTarget;
-			if (input.checked) {
-				myShuffle.filter(input.value);
-			}
-		});
-	}
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    //console.log(scroll);
+    if (scroll > 200) {
+      //console.log('a');
+      $('.navigation').addClass('sticky-header');
+    } else {
+      //console.log('a');
+      $('.navigation').removeClass('sticky-header');
+    }
+  });
 
-	$('.portfolio-single-slider').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
+  /* ========================================================================= */
+  /*	Portfolio Filtering Hook
+  /* =========================================================================  */
 
-	});
+    // filter
+    setTimeout(function(){
+      var containerEl = document.querySelector('.filtr-container');
+      var filterizd;
+      if (containerEl) {
+        filterizd = $('.filtr-container').filterizr({});
+      }
+    }, 500);
 
-	$('.clients-logo').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
-	});
+  /* ========================================================================= */
+  /*	Testimonial Carousel
+  /* =========================================================================  */
 
-	$('.testimonial-slider').slick({
-		slidesToShow: 1,
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
-	});
+  //Init the slider
+  $('.testimonial-slider').slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
 
 
-	// CountDown JS
-	$('.count-down').syotimer({
-		year: 2021,
-		month: 5,
-		day: 9,
-		hour: 20,
-		minute: 30
-	});
+  /* ========================================================================= */
+  /*	Clients Slider Carousel
+  /* =========================================================================  */
 
-	// Magnific Popup Image
-	$('.portfolio-popup').magnificPopup({
-		type: 'image',
-		removalDelay: 160, //delay removal by X to allow out-animation
-		callbacks: {
-			beforeOpen: function () {
-				// just a hack that adds mfp-anim class to markup
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-				this.st.mainClass = this.st.el.attr('data-effect');
-			}
-		},
-		closeOnContentClick: true,
-		midClick: true,
-		fixedContentPos: true,
-		fixedBgPos: true
-	});
-
-	//  Count Up
-	function counter() {
-		var oTop;
-		if ($('.count').length !== 0) {
-			oTop = $('.count').offset().top - window.innerHeight;
-		}
-		if ($(window).scrollTop() > oTop) {
-			$('.count').each(function () {
-				var $this = $(this),
-					countTo = $this.attr('data-count');
-				$({
-					countNum: $this.text()
-				}).animate({
-					countNum: countTo
-				}, {
-					duration: 1000,
-					easing: 'swing',
-					step: function () {
-						$this.text(Math.floor(this.countNum));
-					},
-					complete: function () {
-						$this.text(this.countNum);
-					}
-				});
-			});
-		}
-	}
-	$(window).on('scroll', function () {
-		counter();
-	});
+  //Init the slider
+  $('.clients-logo-slider').slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 5,
+    slidesToScroll: 1
+  });
 
 
 
 
-	// contactr form
-	$('#contact-form').validate({
-		rules: {
-			user_name: {
-				required: true,
-				minlength: 4
-			},
-			user_email: {
-				required: true,
-				email: true
-			},
-			user_subject: {
-				required: false
-			},
-			user_message: {
-				required: true
-			}
-		},
-		messages: {
-			user_name: {
-				required: 'Come on, you have a name don\'t you?',
-				minlength: 'Your name must consist of at least 2 characters'
-			},
-			user_email: {
-				required: 'Please put your email address'
-			},
-			user_message: {
-				required: 'Put some messages here?',
-				minlength: 'Your name must consist of at least 2 characters'
-			}
+  /* ========================================================================= */
+  /*	Company Slider Carousel
+  /* =========================================================================  */
+  $('.company-gallery').slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 5,
+    slidesToScroll: 1
+  });
 
-		},
-		submitHandler: function (form) {
-			$(form).ajaxSubmit({
-				type: 'POST',
-				data: $(form).serialize(),
-				url: 'sendmail.php',
-				success: function () {
-					$('#contact-form #success').fadeIn();
-				},
-				error: function () {
 
-					$('#contact-form #error').fadeIn();
-				}
-			});
-		}
-	});
+  /* ========================================================================= */
+  /*   Contact Form Validating
+  /* ========================================================================= */
 
-});
+  $('#contact-form').validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 4
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        subject: {
+          required: false
+        },
+        message: {
+          required: true
+        }
+      },
+      messages: {
+        user_name: {
+          required: 'Come on, you have a name don\'t you?',
+          minlength: 'Your name must consist of at least 2 characters'
+        },
+        email: {
+          required: 'Please put your email address'
+        },
+        message: {
+          required: 'Put some messages here?',
+          minlength: 'Your name must consist of at least 2 characters'
+        }
+      },
+      submitHandler: function (form) {
+        $(form).ajaxSubmit({
+          type: 'POST',
+          data: $(form).serialize(),
+          url: 'sendmail.php',
+          success: function () {
+            $('#contact-form #success').fadeIn();
+          },
+          error: function () {
+            $('#contact-form #error').fadeIn();
+          }
+        });
+      }
+    }
+
+  );
+
+  /* ========================================================================= */
+  /*	On scroll fade/bounce effect
+  /* ========================================================================= */
+  var scroll = new SmoothScroll('a[href*="#"]');
+
+  // -----------------------------
+  //  Count Up
+  // -----------------------------
+  function counter() {
+    if ($('.counter').length !== 0) {
+      var oTop = $('.counter').offset().top - window.innerHeight;
+    }
+    if ($(window).scrollTop() > oTop) {
+      $('.counter').each(function () {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+          countNum: countTo
+        }, {
+          duration: 1000,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
+          }
+        });
+      });
+    }
+  }
+  // -----------------------------
+  //  On Scroll
+  // -----------------------------
+  $(window).scroll(function () {
+    counter();
+  });
+
+})(jQuery);
